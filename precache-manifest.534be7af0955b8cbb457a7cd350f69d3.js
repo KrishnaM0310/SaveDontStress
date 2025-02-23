b4139cded96c7111fa16462c38ee788b607e2d7d
Krishna <krishnamehta0310@gmail.com>
2025-02-23 08:06:29 -0500

    Updates

---
+++ precache-manifest.534be7af0955b8cbb457a7cd350f69d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afb2f4356a5d18d86ec992d0fdb0b4c7",
+    "url": "/SaveDontStress/index.html"
+  },
+  {
+    "revision": "2ced6fc0a7c5d1725a08",
+    "url": "/SaveDontStress/static/css/main.dd3d7b26.chunk.css"
+  },
+  {
+    "revision": "638e200e69d950262938",
+    "url": "/SaveDontStress/static/js/2.ae026ab9.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/SaveDontStress/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "2ced6fc0a7c5d1725a08",
+    "url": "/SaveDontStress/static/js/main.56add112.chunk.js"
+  },
+  {
+    "revision": "582e6afd5f6b0f4ff2f3",
+    "url": "/SaveDontStress/static/js/runtime~main.bd2a9cb0.js"
+  }
+]);
